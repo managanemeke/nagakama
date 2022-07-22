72fa2c29ca46d92480bd915f7987de2d2b4a477b
managanemeke <managanemeke@gmail.com>
2022-07-22 14:08:50 +0300

    mama

--- nagakama.php
+++ nagakama.php
@@ -279,11 +279,41 @@ function modo(hava){
     //console.log(ha);
   }
 }
+function kopopoko(ma){
+  let textArea = document.createElement("textarea");
+  textArea.style.position = "fixed";
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+  //
+  textArea.style.width = "2em";
+  textArea.style.height = "2em";
+  textArea.style.padding = 0;
+  textArea.style.border = "none";
+  textArea.style.outline = "none";
+  textArea.style.boxShadow = "none";
+  textArea.value = ma;
+  //
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  //
+  try {
+    let successful = document.execCommand("copy");
+    //var msg = successful ? "successful" : "unsuccessful";
+    //console.log("Copying text command was " + msg);
+  } catch (err) {
+    //console.log("Oops, unable to copy");
+  }
+  document.body.removeChild(textArea);
+}
 function kopo(hava){
   hava.preventDefault();
   let ha = hava.currentTarget;
   let kapa = ha.innerHTML;
-  navigator.clipboard.writeText(kapa);
+  //navigator.clipboard.writeText(kapa);
+  ////
+  kopopoko(kapa);
+  ////
 }
 async function sonosono(ma,na){
   let ga = await fetch(na, {
